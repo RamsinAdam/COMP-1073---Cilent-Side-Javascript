7d64887200cc974a75d48a0a618b77525f4cfbc0
RamsinAdam <133426494+RamsinAdam@users.noreply.github.com>
2024-09-24 17:12:25 -0400

    Declared and Initialized Variables

--- Start/js/silly-story.js
+++ Start/js/silly-story.js
@@ -5,10 +5,14 @@
 - randomize for the button
 - story for the paragraph that outputs the final story
 */
+let customName = document.getElementById('name');
+let randomize = document.querySelector('.randomize');
+let story = document.querySelector('.story');
 
 /* STEP 3: Create the variable that contains the story string that will be modified - use var storyText to containt the following:
 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 */
+
 
 /* STEP 4: Create three arrays, insertX, insertY, and insertZ, assigning them the following array values respectively:
 Donald Trump, Jackie Chan, Santa Claus
