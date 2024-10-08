fc2398134ce229fa19c8391dcaf164fa8d682dc5
RamsinAdam <133426494+RamsinAdam@users.noreply.github.com>
2024-10-08 19:35:59 -0400

    Variables that store the selected choices for each part of the sentence

--- Assignment--2/script.js
+++ Assignment--2/script.js
@@ -5,3 +5,9 @@ const actions = ["flies", "jumps", "runs", "dances", "sings"];
 const places = ["on the moon", "in the forest", "in a castle", "on a ship", "in space"];
 const objects = ["with a magic wand", "with a treasure map", "with a sword", "with a book", "with a guitar"];
 const times = ["at midnight", "during the day", "in the morning", "at sunset", "at dawn"];
+// Variables that stores the user's selected choices for each part of the sentence
+let selectedCharacter = '';
+let selectedAction = '';
+let selectedPlace = '';
+let selectedObject = '';
+let selectedTime = '';
